3b30b14b9f8f4e78ea15370369f84ebf74542991
nettib <netsanettibebu098@gmail.com>
2024-07-19 00:39:57 +0300

    Time: 149 ms (6.32%) | Memory: 18.1 MB (7.96%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        j=1
+        while(j<len(nums)):
+            if nums[i]==nums[j]:
+                nums.remove(nums[j])
+                i-=1
+                j-=1
+            i+=1
+            j+=1
+            
+        
