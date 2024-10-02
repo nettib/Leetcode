b1102e34a3c006711d417528185a4cc44c2f2388
nettib <netsanettibebu098@gmail.com>
2024-10-03 00:47:55 +0300

    Time: 174 ms (99.62%) | Memory: 30.4 MB (27.62%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,16 +1,50 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left = 0
-        currentSum = 0
-        minLength = float("inf")
-        for right in range(len(nums)):
-            currentSum+=nums[right]
-            while currentSum>= target:
-                minLength = min(minLength,right-left+1)
-                currentSum -=nums[left]
-                left+=1
-        if minLength == float("inf"):
+        l=0
+        res = float("inf")
+        current = 0
+        for r in range(len(nums)):
+            current+=nums[r]
+            while current >= target:
+                res=min(res,r-l+1)
+                current-=nums[l]
+                l+=1
+        if res == float("inf"):
             return 0
-        else:
-            return minLength
+        return res
+            
+        
+        
+        # res=float("inf")
+        # l=0
+        # r=0
+        # flag = False
+        # while l <= r and r < len(nums):
+        #     arr = nums[l:r + 1]
+        #     current = sum(arr)
+        #     if current >= target:
+        #         res=min(res,len(arr))
+        #         flag = True
+        #         l+=1
+        #     else:
+        #         r+=1
+        # if flag == True:
+        #     return res
+        # return 0
+
+
+
+        # left = 0
+        # currentSum = 0
+        # minLength = float("inf")
+        # for right in range(len(nums)):
+        #     currentSum+=nums[right]
+        #     while currentSum>= target:
+        #         minLength = min(minLength,right-left+1)
+        #         currentSum -=nums[left]
+        #         left+=1
+        # if minLength == float("inf"):
+        #     return 0
+        # else:
+        #     return minLength
 
