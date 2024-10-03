92580c72d4755c828ed780415c3cf0af446f17d0
nettib <netsanettibebu098@gmail.com>
2024-10-04 00:19:57 +0300

    Time: 79 ms (80.26%) | Memory: 17.8 MB (58.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,18 +1,53 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        @cache
-        def checkAnagram(word):
-            if sorted(p) == sorted(word):
-                return True
-            return False
-        left = 0 
-        current = s[left:len(p)]
-        ans = []
-        for right in range(len(p),len(s)):
-            if checkAnagram(current):
-                ans.append(left)
-            left+=1
-            current = s[left:right+1]
-        if checkAnagram(current):
-            ans.append(left)
-        return ans
+        def createDic(word):
+            freq={}
+            for char in word:
+                if char in freq:
+                    freq[char]+=1
+                else:
+                    freq[char]=1
+            return freq
+        p_freq = createDic(p)
+        s_freq = {}
+        res = []
+
+        for i in range(len(s)):
+            if s[i] in s_freq:
+                s_freq[s[i]]+=1
+            else:
+                s_freq[s[i]]=1
+
+            if i >= len(p):
+                left = s[i-len(p)]
+                if s_freq[left] == 1:
+                    del s_freq[left]
+                else:
+                    s_freq[left]-=1
+
+            if s_freq == p_freq:
+                res.append(i - len(p) + 1)
+        return res
+
+        
+        
+        
+        
+        
+        
+        # @cache
+        # def checkAnagram(word):
+        #     if sorted(p) == sorted(word):
+        #         return True
+        #     return False
+        # left = 0 
+        # current = s[left:len(p)]
+        # ans = []
+        # for right in range(len(p),len(s)):
+        #     if checkAnagram(current):
+        #         ans.append(left)
+        #     left+=1
+        #     current = s[left:right+1]
+        # if checkAnagram(current):
+        #     ans.append(left)
+        # return ans
