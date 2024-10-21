58759c2958e854247057be43a469cefea8ebda96
nettib <netsanettibebu098@gmail.com>
2024-10-21 23:52:15 +0300

    Time: 3 ms (96.56%) | Memory: 16.6 MB (24.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,26 +1,45 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dic={')':'(',']':'[','}':'{'}
-        stack=[]
-        if s[0] in dic.keys():
-            return False 
-        for char in s:
-            if char in dic.values():
-                stack.append(char)
+        arr = []
+        for i in range(len(s)):
+            if s[i] in "([{":
+                arr.append(s[i])
             else:
-                if len(stack)!=0:
-                    if dic[char]!=stack[-1]:
-                        return False
+                if len(arr) != 0:
+                    if s[i] == ')' and arr[-1] == "(":
+                        arr.pop()
+                    elif s[i] == "]" and arr[-1] == "[":
+                        arr.pop()
+                    elif s[i] == "}" and arr[-1] == "{":
+                        arr.pop()
                     else:
-                        stack.pop()
+                        arr.append(s[i])
                 else:
-                    if dic[char]:
-                        if len(stack)==0:
-                            return False
-        if len(stack)==0:
-            return True
-        else:
-            return False
-        print(stack)
+                    arr.append(s[i])
+
+        return len(arr) == 0
+
+        # dic={')':'(',']':'[','}':'{'}
+        # stack=[]
+        # if s[0] in dic.keys():
+        #     return False 
+        # for char in s:
+        #     if char in dic.values():
+        #         stack.append(char)
+        #     else:
+        #         if len(stack)!=0:
+        #             if dic[char]!=stack[-1]:
+        #                 return False
+        #             else:
+        #                 stack.pop()
+        #         else:
+        #             if dic[char]:
+        #                 if len(stack)==0:
+        #                     return False
+        # if len(stack)==0:
+        #     return True
+        # else:
+        #     return False
+        # print(stack)
         
         
