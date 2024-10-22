bb8343ef01e7218f9885840679834bdc221a74a9
nettib <netsanettibebu098@gmail.com>
2024-10-23 01:05:25 +0300

    Time: 5 ms (95.27%) | Memory: 18.3 MB (5.69%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -2,14 +2,42 @@ class Solution:
    def evalRPN(self, tokens: List[str]) -> int:
     stack = []
     for i in range(len(tokens)):
-        if tokens[i] in ['+', '-', '*', '/']:
-            b = stack.pop()
-            a = stack.pop()
-            res = eval(f"{a}{tokens[i]}{b}")
-            stack.append(int(res))
+        if tokens[i] not in "+-*/":
+            stack.append(int(tokens[i]))
+        elif tokens[i] == "+":
+            if len(stack) > 1:
+                a = stack[-2] + stack[-1]
+                del stack[len(stack) - 2:len(stack)]
+                stack.append(int(a))
+        elif tokens[i] == "-":
+            if len(stack) > 1:
+                a = stack[-2] - stack[-1]
+                del stack[len(stack) - 2:len(stack)]
+                stack.append(int(a))
+        elif tokens[i] == "*":
+            if len(stack) > 1:
+                a = stack[-2] * stack[-1]
+                del stack[len(stack) - 2:len(stack)]
+                stack.append(int(a))
         else:
-            stack.append(tokens[i])
-    return int(stack[0])
+            if len(stack) > 1:
+                a = stack[-2] / stack[-1]
+                del stack[len(stack) - 2:len(stack)]
+                stack.append(int(a))
+    return stack[0]
+
+
+
+    # stack = []
+    # for i in range(len(tokens)):
+    #     if tokens[i] in ['+', '-', '*', '/']:
+    #         b = stack.pop()
+    #         a = stack.pop()
+    #         res = eval(f"{a}{tokens[i]}{b}")
+    #         stack.append(int(res))
+    #     else:
+    #         stack.append(tokens[i])
+    # return int(stack[0])
 
 
         
