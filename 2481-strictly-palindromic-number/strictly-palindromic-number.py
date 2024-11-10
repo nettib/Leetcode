970bda8a8969785a1291e3a72fc5788dd3d72964
nettib <netsanettibebu098@gmail.com>
2024-11-11 00:57:38 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (14.41%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.py
@@ -1,16 +1,18 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        def basic(num,b):
-            if num == 0:
-                return "0"
-            digits = []
-            while num > 0:
-                digits.append(str(num%b))
-                num//=b
-            return "".join(digits[::-1])
-        for i in range(2,n-1):
-            pal = basic(n,i)
-            if pal != pal[::-1]:
-                return False
-        return True
+        return False
+        # def basic(num,b):
+        #     if num == 0:
+        #         return "0"
+        #     digits = []
+        #     while num > 0:
+        #         digits.append(str(num%b))
+        #         num//=b
+        #     return "".join(digits[::-1])
+        # for i in range(2,n-1):
+        #     pal = basic(n,i)
+        #     if pal != pal[::-1]:
+        #         return False
+        # return True
+        
 
