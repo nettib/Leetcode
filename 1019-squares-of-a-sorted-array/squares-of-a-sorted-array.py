d020bad704b133d57638cf35096367ca86b553c5
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-03-02 16:25:19 +0300

    Time: 8 ms (67.35%) | Memory: 19.3 MB (89.12%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,7 +1,31 @@
 class Solution:
+    # def sortedSquares(self, nums: List[int]) -> List[int]:
+    #     for index in range(len(nums)):
+    #         nums[index] = nums[index]**2
+    #     nums.sort()
+    #     return nums
+    #Bruteforce Solution
+    # def sortedSquares(self, nums: List[int]) -> List[int]:
+    #     l, r = 0, len(nums) - 1
+    #     while l < r:
+    #         firstSquare = nums[l] ** 2
+    #         secondSquare = nums[r] ** 2
+    #         if firstSquare > secondSquare:
+    #             nums[l], nums[r] = nums[r], nums[l] ** 2
+    #             l+=1
+    #         r-=1
+    #     return nums
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        for index in range(len(nums)):
-            nums[index] = nums[index]**2
-        nums.sort()
-        return nums
+        l, r = 0, len(nums) - 1
+        result = []
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                result.append(nums[l]**2)
+                l+=1
+            else:
+                result.append(nums[r]**2)
+                r-=1
+        result.reverse()
+        return result
+
         
