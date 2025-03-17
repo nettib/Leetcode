ba628707a83aff441e69b4c7440a14f0b20d98fc
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-03-17 13:17:31 +0300

    Time: 1991 ms (5.03%) | Memory: 20.7 MB (98.88%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,22 +1,56 @@
+# Using Dictionary
+# class MyHashMap:
+
+#     def __init__(self):
+#         self.hashmap = {}
+
+#     def put(self, key: int, value: int) -> None:
+#         self.hashmap[key] = value
+        
+
+#     def get(self, key: int) -> int:
+#         if key in self.hashmap:
+#             return self.hashmap[key]
+#         else:
+#             return -1
+        
+
+#     def remove(self, key: int) -> None:
+#         if key in self.hashmap:
+#             del self.hashmap[key]
+
+
 class MyHashMap:
 
     def __init__(self):
-        self.hashmap = {}
+        self.hashmap = []
 
     def put(self, key: int, value: int) -> None:
-        self.hashmap[key] = value
-        
-
+        isKey = False
+        if len(self.hashmap) == 0:
+            self.hashmap.append([key, value])
+        for i in range(len(self.hashmap)):
+            if key == self.hashmap[i][0]:
+                self.hashmap[i][1] = value
+                isKey = True
+                return
+        if not isKey:
+            self.hashmap.append([key, value])
+        # print(self.hashmap)
     def get(self, key: int) -> int:
-        if key in self.hashmap:
-            return self.hashmap[key]
-        else:
-            return -1
-        
+        for i in range(len(self.hashmap)):
+            if key == self.hashmap[i][0]:
+                return self.hashmap[i][1]
+        return -1
 
     def remove(self, key: int) -> None:
-        if key in self.hashmap:
-            del self.hashmap[key]
+        for i in range(len(self.hashmap)):
+            if key == self.hashmap[i][0]:
+                self.hashmap.remove(self.hashmap[i])
+                return
+
+        
+
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
