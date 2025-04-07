8da35a267eadee240814f9a2df0517fb1ca55251
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-04-08 02:44:36 +0300

    Time: 46 ms (74.66%) | Memory: 23.2 MB (85.35%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,11 +1,5 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        # 1    5       0 3 
-        #    lr          l r
-        # [1,2,2,3]     [1,2]
-        # 0      5
-        # lr
-        # [3,3,4,5]
         people.sort()
         l, r = 0, len(people) - 1
         boats = 0
