1a3a09b52516a5aa81ffa15d79b28f8eb0e1a476
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-04-27 06:36:19 +0300

    Time: 2 ms (35.13%) | Memory: 17.6 MB (99.48%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,28 +1,13 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
         l = 0
-        for r in range(l+1, len(nums)):
-            if nums[l] == nums[r]:
-                nums[l]*=2
-                nums[r] = 0
-            l+=1
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i+=1
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
         return nums
-            # for i in range(len(nums) - 1):
-            #     if nums[i] == nums[i + 1]:
-            #         nums[i] = nums[i] * 2
-            #         nums[i + 1] = 0
-            # nonZero=[]
-            # zero=0
-            # for itm in nums:
-            #     if itm!=0:
-            #         nonZero.append(itm)
-            #     else:
-            #         zero+=1
-            # nonZero.extend([0]*zero)
-            # return nonZero
-            
+
