2339a1e34557578e0d38e165b4df1c2a4b3759f2
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-04-28 23:30:58 +0300

    Time: 7 ms (44.93%) | Memory: 25.6 MB (46.62%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,44 +1,19 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k % n  # Handle cases where k is greater than the length of the array
-
-        # Helper function to reverse a portion of the array
-        def reverse(start: int, end: int) -> None:
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-
-        # Step 1: Reverse the entire array
-        reverse(0, n - 1)
-
-        # Step 2: Reverse the first k elements
-        reverse(0, k - 1)
-
-        # Step 3: Reverse the rest of the array
-        reverse(k, n - 1)
-
-        # for i in range(0,k):
-        #     nums.insert(0,nums[len(nums)-1])
-        #     nums.pop()
-
-
-
-
-
-
-
-
-
-
-        # flag = 0
-        # while flag < k:
-        #     nums.insert(0,nums[len(nums)-1])
-        #     nums.pop()
-        #     flag+=1
-
         """
         Do not return anything, modify nums in-place instead.
         """
+        if k == 0:
+            return nums
+        k = k % len(nums)
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
+        return nums
+
+
         
