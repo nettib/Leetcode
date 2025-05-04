a00c0720bcf13ca78d73069ba104b8323b13d25d
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-05-04 12:41:05 +0300

    Time: 22 ms (34.66%) | Memory: 17.8 MB (52.97%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,41 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        long_substring = 1
         l = 0
-        ans = 0
-        for r in range(len(s)):
-            while s[r] in s[l:r]:
-                l+=1
-            ans = max(ans, r - l + 1)
-        return ans
-
-
-
-
-        # char_set = set()  # To store characters in the current window
-        # left = 0  # Left pointer
-        # longest = 0  # Length of the longest substring
-
-        # for right in range(len(s)):
-        #     # If we encounter a duplicate, move the left pointer
-        #     while s[right] in char_set:
-        #         char_set.remove(s[left])
-        #         left += 1
-        #     # Add the current character to the set
-        #     char_set.add(s[right])
-        #     # Update the maximum length found
-        #     longest = max(longest, right - left + 1)
-
-        # return longest
-        
-        # if len(s) == 0:
-        #     return 0
-        # arr = []
-        # longest = 1
-        # for r in range(0, len(s)):
-        #     while s[r] in arr:
-        #         if len(arr) != 0:
-        #             arr.remove(arr[0])
-        #             longest = max(longest, len(arr))
-        #     arr.append(s[r])
-        #     longest = max(longest, len(arr))
-        # return longest
+        for r in range(1, len(s)):
+            while l < r and s[r] in s[l:r]:
+                l += 1
+            long_substring = max(long_substring, r - l + 1)
+        return long_substring
