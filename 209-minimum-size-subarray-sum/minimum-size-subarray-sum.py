c32bc4990b309b6644ddea4528e15e3e1199cf4f
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-05-05 19:03:32 +0300

    Time: 7 ms (91.97%) | Memory: 28.4 MB (11.14%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,68 +1,21 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         l = 0
-        min_length = float("inf")
-        if nums[l] >= target:
-            return 1
-        r = l + 1
-        total = 0
+        current = 0
+        min_size = float("inf")
+
         for r in range(len(nums)):
-            total+=nums[r]
-            while total >= target:
-                min_length = min(min_length, r - l + 1)
-                total-=nums[l]
-                l+=1
-        if min_length == float("inf"):
+            current += nums[r]
+            if current >= target:
+                min_size = min(min_size, r - l + 1)
+            while current >= target:
+                current -= nums[l]
+                l += 1
+                if current >= target:
+                    min_size = min(min_size, r - l + 1)
+        
+        if min_size == float("inf"):
             return 0
-        return min_length
+        return min_size
 
-
-
-        # l=0
-        # res = float("inf")
-        # current = 0
-        # for r in range(len(nums)):
-        #     current+=nums[r]
-        #     while current >= target:
-        #         res=min(res,r-l+1)
-        #         current-=nums[l]
-        #         l+=1
-        # if res == float("inf"):
-        #     return 0
-        # return res
-            
-        
-        
-        # res=float("inf")
-        # l=0
-        # r=0
-        # flag = False
-        # while l <= r and r < len(nums):
-        #     arr = nums[l:r + 1]
-        #     current = sum(arr)
-        #     if current >= target:
-        #         res=min(res,len(arr))
-        #         flag = True
-        #         l+=1
-        #     else:
-        #         r+=1
-        # if flag == True:
-        #     return res
-        # return 0
-
-
-
-        # left = 0
-        # currentSum = 0
-        # minLength = float("inf")
-        # for right in range(len(nums)):
-        #     currentSum+=nums[right]
-        #     while currentSum>= target:
-        #         minLength = min(minLength,right-left+1)
-        #         currentSum -=nums[left]
-        #         left+=1
-        # if minLength == float("inf"):
-        #     return 0
-        # else:
-        #     return minLength
 
