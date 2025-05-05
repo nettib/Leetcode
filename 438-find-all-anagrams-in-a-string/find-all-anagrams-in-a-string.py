647bc87e18d76a4d3aa7d1cd7dc635b519c10da6
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-05-05 18:38:39 +0300

    Time: 40 ms (70.09%) | Memory: 18.4 MB (36.51%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,85 +1,40 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        def frequency(word):
-            freq = {}
-            for char in word:
-                if char in freq:
-                    freq[char]+=1
-                else:
-                    freq[char] = 1
-            return freq
-        p_freq = frequency(p)
-        s_freq = {}
-        ans = []
-        l = 0
-        for r in range(len(s)):
-            if s[r] in s_freq:
-                s_freq[s[r]]+=1
+        s_hash = {}
+        p_hash = {}
+        k = len(p)
+        res = []
+
+        if k > len(s):
+            return res
+
+        for i in range(k):
+            if p[i] in p_hash:
+                p_hash[p[i]] += 1
             else:
-                s_freq[s[r]] = 1
-
-            if r >= len(p) - 1:
-                if s_freq == p_freq:
-                    ans.append(l)
-                if s_freq[s[l]] == 1:
-                    del s_freq[s[l]]
-                else:
-                    s_freq[s[l]]-=1
-                l+=1
-
-        return ans
-
-
-
-
-        # def createDic(word):
-        #     freq={}
-        #     for char in word:
-        #         if char in freq:
-        #             freq[char]+=1
-        #         else:
-        #             freq[char]=1
-        #     return freq
-        # p_freq = createDic(p)
-        # s_freq = {}
-        # res = []
-
-        # for i in range(len(s)):
-        #     if s[i] in s_freq:
-        #         s_freq[s[i]]+=1
-        #     else:
-        #         s_freq[s[i]]=1
-
-        #     if i >= len(p):
-        #         left = s[i-len(p)]
-        #         if s_freq[left] == 1:
-        #             del s_freq[left]
-        #         else:
-        #             s_freq[left]-=1
-
-        #     if s_freq == p_freq:
-        #         res.append(i - len(p) + 1)
-        # return res
-
+                p_hash[p[i]] = 1
         
+        for i in range(k):
+            if s[i] in s_hash:
+                s_hash[s[i]] += 1
+            else:
+                s_hash[s[i]] = 1
+            
+        if s_hash == p_hash:
+            res.append(0)
+
+        for i in range(k, len(s)):
+            s_hash[s[i - k]] -= 1
+            if s[i] in s_hash:
+                s_hash[s[i]] += 1
+            else:
+                s_hash[s[i]] = 1
+
+            if s_hash[s[i - k]] == 0:
+                del s_hash[s[i - k]]
+
+            if s_hash == p_hash:
+                res.append((i - k) + 1)
+
+        return res
         
-        
-        
-        
-        
-        # @cache
-        # def checkAnagram(word):
-        #     if sorted(p) == sorted(word):
-        #         return True
-        #     return False
-        # left = 0 
-        # current = s[left:len(p)]
-        # ans = []
-        # for right in range(len(p),len(s)):
-        #     if checkAnagram(current):
-        #         ans.append(left)
-        #     left+=1
-        #     current = s[left:right+1]
-        # if checkAnagram(current):
-        #     ans.append(left)
-        # return ans
