03f619bf9a923ba4c32ba865cc5ec75ae98781db
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-05-07 23:14:16 +0300

    Time: 19 ms (67.09%) | Memory: 18 MB (18.91%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,50 +1,28 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        def createDic(word):
-            freq = {}
-            for char in word:
-                if char in freq:
-                    freq[char] += 1
-                else:
-                    freq[char] = 1
-            return freq
-
-        s1_freq = createDic(s1)
+        if len(s1) > len(s2):
+            return False
+        s1_freq = {}
         s2_freq = {}
 
-        for right in range(len(s2)):
-            if s2[right] in s2_freq:
-                s2_freq[s2[right]]+=1
-            else:
-                s2_freq[s2[right]] = 1
+        for i in range(len(s1)):
+            s1_freq[s1[i]] = 1 + s1_freq.get(s1[i], 0)
 
-            if right >= len(s1):
-                left = s2[right-len(s1)]
-                if s2_freq[left] == 1:
-                    del s2_freq[left]
-                else:
-                    s2_freq[left] -= 1
+        for i in range(len(s1)):
+            s2_freq[s2[i]] = 1 + s2_freq.get(s2[i], 0)
+        
+        if s2_freq == s1_freq:
+            return True
 
+        for i in range(len(s1), len(s2)):
+            s2_freq[s2[i]] = 1 + s2_freq.get(s2[i], 0)
+            s2_freq[s2[i - len(s1)]] -= 1
+
+            if s2_freq[s2[i - len(s1)]] == 0:
+                del s2_freq[s2[i - len(s1)]]
+            
             if s1_freq == s2_freq:
                 return True
+
         return False
 
-
-        # @cache
-        # def checking(word):
-        #     if sorted(word) == sorted(s1):
-        #         return True
-        # l=0
-        # r=len(s1)
-        # while r < len(s2):
-        #     current = s2[l:r]
-        #     print(current)
-        #     if checking(current):
-        #         return True 
-        #         break
-        #     l+=1
-        #     r+=1
-        # return False
-            
-
-        
