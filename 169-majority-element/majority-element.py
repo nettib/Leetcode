0bde4a1a58c3800b54edaeef0d3816dfdfd73415
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-02 01:09:21 +0300

    Time: 15 ms (15.40%) | Memory: 19.4 MB (50.99%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = {}
-        major = 0
 
         for num in nums:
             if num not in count:
                 count[num] = 0
             count[num] += 1
 
-        for num in count:
-            if major == 0 or count[num] > count[major]:
+        major = None
+        for num in nums:
+            if major == None or count[num] > count[major]:
                 major = num
-        
+
         return major
