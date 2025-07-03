d6788913bbcf68902fd6d32e0b70f516226e5a92
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-03 04:37:01 +0300

    Time: 15 ms (11.01%) | Memory: 22.3 MB (48.98%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,16 +1,13 @@
-import math
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = {}
+        count = defaultdict(int)
 
         for num in nums:
-            if num not in count:
-                count[num] = 0
             count[num] += 1
 
         major = []
-        for num in count:
-            if count[num] > math.floor(len(nums) / 3):
+        for num in nums:
+            if count[num] > len(nums) // 3 and num not in major:
                 major.append(num)
 
         return major
