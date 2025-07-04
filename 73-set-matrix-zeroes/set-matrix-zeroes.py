47082156b2016429686365136fd981c5168f1fbc
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-05 00:40:08 +0300

    Time: 7 ms (18.76%) | Memory: 18.5 MB (52.84%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,25 +4,27 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
 
-        rows = [1] * len(matrix)
-        columns = [1] * len(matrix[0])
+        # rows = [1] * len(matrix)
+        # columns = [1] * len(matrix[0])
+        left = 1
 
         for row in range(len(matrix)):
             for column in range(len(matrix[row])):
                 if matrix[row][column] == 0:
-                    rows[row] = 0
-                    columns[column] = 0
+                    matrix[row][0] = 0
+                    if column == 0:
+                        left = 0
+                    else:
+                        matrix[0][column] = 0
+        
 
-        for row in range(len(matrix)):
-            if rows[row] == 0:
-                for column in range(len(matrix[row])):
-                    matrix[row][column] = 0
+        for row in range(len(matrix) - 1, -1, -1):
+            for column in range(len(matrix[row]) - 1, -1, -1):
+                if column == 0:
+                    if matrix[row][0] == 0 or left == 0:
+                        matrix[row][column] = 0
+                else:
+                    if matrix[row][0] == 0 or matrix[0][column] == 0:
+                        matrix[row][column] = 0
             
-        for row in range(len(matrix)):
-            for column in range(len(matrix[row])):
-                if columns[column] == 0:
-                    matrix[row][column] = 0
-
-
-
         
