f1ce62fe3a990a828f2bf948850dff76c320c5db
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-08 01:04:18 +0300

    Time: 155 ms (61.63%) | Memory: 18.7 MB (73.15%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,17 +1,23 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        rows, columns = len(img), len(img[0])
-        res = [[0] * columns for _ in range(rows)]
+        rows = len(img)
+        columns = len(img[0])
+
+        matrix = [[0] * columns for _ in range(rows)]
 
         for r in range(rows):
             for c in range(columns):
-                total, cnt = 0, 0
+                total = 0
+                count = 0
                 for i in range(r - 1, r + 2):
+                    if i < 0 or i >= rows:
+                        continue
                     for j in range(c - 1, c + 2):
-                        if i < 0 or i == rows or j < 0 or j == columns:
+                        if j < 0 or j >= columns:
                             continue
                         total += img[i][j]
-                        cnt += 1
-                res[r][c] = total // cnt
+                        count += 1
+                
+                matrix[r][c] = total // count
 
-        return res
+        return matrix
