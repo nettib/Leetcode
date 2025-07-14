60e8bae3e3deb81e6f40d9752e0f199b49a2d31d
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-14 23:22:51 +0300

    Time: 40 ms (69.95%) | Memory: 18.4 MB (56.89%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,40 +1,36 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        s_hash = {}
+        if len(p) > len(s):
+            return []
         p_hash = {}
-        k = len(p)
+        s_hash = {}
+
         res = []
 
-        if k > len(s):
-            return res
+        for char in p:
+            if char not in p_hash:
+                p_hash[char] = 0
+            p_hash[char] += 1
 
-        for i in range(k):
-            if p[i] in p_hash:
-                p_hash[p[i]] += 1
-            else:
-                p_hash[p[i]] = 1
-        
-        for i in range(k):
-            if s[i] in s_hash:
-                s_hash[s[i]] += 1
-            else:
-                s_hash[s[i]] = 1
-            
+        for i in range(len(p)):
+            if s[i] not in s_hash:
+                s_hash[s[i]] = 0
+            s_hash[s[i]] += 1
+
         if s_hash == p_hash:
             res.append(0)
 
-        for i in range(k, len(s)):
-            s_hash[s[i - k]] -= 1
-            if s[i] in s_hash:
-                s_hash[s[i]] += 1
+        for i in range(len(p), len(s)):
+            if s_hash[s[i - len(p)]] == 1:
+                del s_hash[s[i - len(p)]]
             else:
-                s_hash[s[i]] = 1
-
-            if s_hash[s[i - k]] == 0:
-                del s_hash[s[i - k]]
+                s_hash[s[i - len(p)]] -= 1
+            
+            if s[i] not in s_hash:
+                s_hash[s[i]] = 0
+            s_hash[s[i]] += 1
 
             if s_hash == p_hash:
-                res.append((i - k) + 1)
-
-        return res
+                res.append(i - len(p) + 1)
         
+        return res
