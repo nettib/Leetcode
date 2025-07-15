020e166652a2a7e22d59eeaec6a682f4824f0673
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-16 02:34:14 +0300

    Time: 19 ms (66.57%) | Memory: 18.1 MB (19.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,27 +2,27 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        s1_freq = {}
-        s2_freq = {}
+
+        s1_count = {}
+        s2_count = {}
+
+        for char in s1:
+            s1_count[char] = 1 + s1_count.get(char, 0)
 
         for i in range(len(s1)):
-            s1_freq[s1[i]] = 1 + s1_freq.get(s1[i], 0)
-
-        for i in range(len(s1)):
-            s2_freq[s2[i]] = 1 + s2_freq.get(s2[i], 0)
+            s2_count[s2[i]] = 1 + s2_count.get(s2[i], 0)
         
-        if s2_freq == s1_freq:
+        if s1_count == s2_count:
             return True
 
+        
         for i in range(len(s1), len(s2)):
-            s2_freq[s2[i]] = 1 + s2_freq.get(s2[i], 0)
-            s2_freq[s2[i - len(s1)]] -= 1
+            s2_count[s2[i]] = 1 + s2_count.get(s2[i], 0)
+            s2_count[s2[i - len(s1)]] -= 1
+            if s2_count[s2[i - len(s1)]] == 0:
+                del s2_count[s2[i - len(s1)]]
 
-            if s2_freq[s2[i - len(s1)]] == 0:
-                del s2_freq[s2[i - len(s1)]]
-            
-            if s1_freq == s2_freq:
+            if s1_count == s2_count:
                 return True
-
+        
         return False
-
