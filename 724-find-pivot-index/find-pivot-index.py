71bca81bfd499079f3a26a3474252361e77816e7
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-24 23:06:27 +0300

    Time: 11 ms (31.94%) | Memory: 18.9 MB (13.57%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -2,12 +2,12 @@ class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
-        
+
         if nums[len(nums) - 1] - nums[0] == 0:
             return 0
         
         for i in range(1, len(nums) - 1):
-            if nums[len(nums) - 1] - nums[i] == nums[i - 1]:
+            if nums[i - 1] == (nums[len(nums) - 1] - nums[i]):
                 return i
         
         if nums[len(nums) - 2] == 0:
