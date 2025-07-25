4f8c558d08c5e44e72ccc95be5722c86a73d0214
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-25 05:12:20 +0300

    Time: 31 ms (63.78%) | Memory: 20.3 MB (93.51%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,7 +1,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        count = { 0 : 1 }
         res = 0
-        count = {0: 1}
 
         prefix = 0
         for i in range(len(nums)):
@@ -9,5 +9,5 @@ class Solution:
             if prefix - k in count:
                 res += count[prefix - k]
             count[prefix] = count.get(prefix, 0) + 1
-
+        
         return res
