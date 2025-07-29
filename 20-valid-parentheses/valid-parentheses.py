5a3ec57dd48aa3ade47fe6ad703bee975d83b2ff
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-07-29 23:58:38 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (9.98%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,45 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        arr = []
-        for i in range(len(s)):
-            if s[i] in "([{":
-                arr.append(s[i])
+        pair = { '(': ')', '{': '}', '[': ']' }
+        stack = []
+
+        for bracket in s:
+            if bracket in pair:
+                stack.append(bracket)
+            elif len(stack) != 0 and bracket == pair[stack[len(stack) - 1]]:
+                stack.pop()
             else:
-                if len(arr) != 0:
-                    if s[i] == ')' and arr[-1] == "(":
-                        arr.pop()
-                    elif s[i] == "]" and arr[-1] == "[":
-                        arr.pop()
-                    elif s[i] == "}" and arr[-1] == "{":
-                        arr.pop()
-                    else:
-                        arr.append(s[i])
-                else:
-                    arr.append(s[i])
-
-        return len(arr) == 0
-
-        # dic={')':'(',']':'[','}':'{'}
-        # stack=[]
-        # if s[0] in dic.keys():
-        #     return False 
-        # for char in s:
-        #     if char in dic.values():
-        #         stack.append(char)
-        #     else:
-        #         if len(stack)!=0:
-        #             if dic[char]!=stack[-1]:
-        #                 return False
-        #             else:
-        #                 stack.pop()
-        #         else:
-        #             if dic[char]:
-        #                 if len(stack)==0:
-        #                     return False
-        # if len(stack)==0:
-        #     return True
-        # else:
-        #     return False
-        # print(stack)
-        
-        
+                return False
+            
+        return len(stack) == 0
