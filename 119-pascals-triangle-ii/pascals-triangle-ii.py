5bc189cc9e144ad9e585aa39658da6e821c5d98c
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-08-12 03:30:04 +0300

    Time: 3 ms (9.71%) | Memory: 17.7 MB (51.26%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,12 +1,24 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        # My Solution
+        # res = [1]
+
+        # for i in range(rowIndex):
+        #     nums = res.copy()
+        #     for j in range(1, len(nums)):
+        #         nums[j] = res[j] + res[j - 1]
+        #     nums.append(1)
+        #     res = nums
+
+        # return res
+
         res = [1]
 
         for i in range(rowIndex):
-            nums = res.copy()
-            for j in range(1, len(nums)):
-                nums[j] = res[j] + res[j - 1]
-            nums.append(1)
+            nums = [0] * (len(res) + 1)
+            for j in range(len(res)):
+                nums[j] += res[j]
+                nums[j + 1] += res[j]
             res = nums
-
+        
         return res
