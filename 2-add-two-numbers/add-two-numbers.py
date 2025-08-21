e9c2a4d032c168d02dddee5b953e08f22aacf501
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-08-21 08:39:10 +0300

    Time: 3 ms (72.57%) | Memory: 17.7 MB (84.71%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,27 +5,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = ""
-        num2 = ""
-
-        curr = l1
-        while curr:
-            num1 += str(curr.val)
-            curr = curr.next
-        
-        curr = l2
-        while curr:
-            num2 += str(curr.val)
-            curr = curr.next
-        
-        num1 = str(int(num1[::-1]) + int(num2[::-1]))
         dummy = ListNode()
+
         curr = dummy
-        for i in range(len(num1) - 1, -1, -1):
-            curr.next = ListNode(int(num1[i]))
+        stand = 0
+        while l1 and l2:
+            curr.next = ListNode(((l1.val + l2.val)  + stand) % 10)
+            stand = (l1.val + l2.val + stand) // 10
             curr = curr.next
+            l1 = l1.next
+            l2 = l2.next
+        
+        while l1:
+            curr.next = ListNode(((l1.val)  + stand) % 10)
+            stand = (l1.val + stand) // 10
+            curr = curr.next
+            l1 = l1.next
+        
+        while l2:
+            curr.next = ListNode(((l2.val)  + stand) % 10)
+            stand = (l2.val + stand) // 10
+            curr = curr.next
+            l2 = l2.next
+        
+        if stand:
+            curr.next = ListNode(stand)
         
         return dummy.next
-
-
-        
