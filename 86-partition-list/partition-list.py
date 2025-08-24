59fb081739d5712588252b62b9f2917bdddaf534
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-08-24 13:28:15 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.40%) - LeetSync

---
+++ 86-partition-list/partition-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        dummy1 = ListNode()
+        dummy2 = ListNode()
+
+        curr1 = dummy1
+        curr2 = dummy2
+        curr = head
+
+        while curr:
+            if curr.val < x:
+                curr1.next = curr
+                curr = curr.next
+                curr1 = curr1.next
+                curr1.next = None
+            else:
+                curr2.next = curr
+                curr = curr.next
+                curr2 = curr2.next
+                curr2.next = None
+        
+        curr1.next = dummy2.next
+        dummy2.next = None
+
+        return dummy1.next
+        
