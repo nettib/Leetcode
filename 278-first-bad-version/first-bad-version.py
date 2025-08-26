51953e4c40e99e8d8bee2cc4f8eed603075e4657
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-08-26 07:14:50 +0300

    Time: 41 ms (29.43%) | Memory: 17.7 MB (43.66%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -4,14 +4,11 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         v1, v2 = 1, n
-        while v1 <= v2:
+        while v1 < v2:
             vm = v1 + ((v2 - v1) // 2)
-            print(vm)
-            if isBadVersion(vm) and v1 == vm == v2:
-                return vm
-            elif isBadVersion(vm):
+            if isBadVersion(vm):
                 v2 = vm
             else:
                 v1 = vm + 1
 
-        
+        return v1
