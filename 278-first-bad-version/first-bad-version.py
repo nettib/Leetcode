ed7422f61456c232cd5be81ec77ac6cd56c769fc
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-08-30 18:08:46 +0300

    Time: 40 ms (38.54%) | Memory: 17.9 MB (26.40%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,15 +3,13 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        firstBad = 0
         l, r = 1, n
 
         while l <= r:
             m = l + ((r - l) // 2)
             if isBadVersion(m):
-                firstBad = m
                 r = m - 1
             else:
                 l = m + 1
         
-        return firstBad
+        return l
