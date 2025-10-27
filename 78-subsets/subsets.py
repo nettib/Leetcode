31ceda33dac0b86eea81778f76da7af8f15409ea
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-10-27 23:18:52 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (39.69%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -6,12 +6,11 @@ class Solution:
             if i >= len(nums):
                 res.append(path[:])
                 return
-            
 
             path.append(nums[i])
             backtrack(i + 1, path)
             path.pop()
             backtrack(i + 1, path)
-        
+
         backtrack(0, [])
         return res
