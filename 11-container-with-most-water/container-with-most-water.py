5b6463495542d744d274dfd71cd8a6d88f0d8428
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-07 00:39:05 +0300

    Time: 119 ms (34.62%) | Memory: 20.8 MB (55.07%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,34 +1,19 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        # maxArea = 0
-        # for i in range(len(height)):
-        #     for j in range(len(height)):
-        #         width = j-i
-        #         length = min(height[i],height[j])
-        #         area = width*length
-        #         maxArea = max(maxArea,area)
-        # return maxArea 
-        maxarea = 0
-        l=0
-        r=len(height) - 1
-        while l <= r:
-            width = r - l
-            length = min(height[l],height[r])
-            area = width*length
-            maxarea = max(maxarea,area)
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+        water = 0
+
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            water = max(water, area)
+
             if height[l] < height[r]:
-                l+=1
-            elif height[l] > height[r]:
-                r-=1
+                l += 1
             else:
-                if r - l > 1:
-                    if height[l+1] > height[r-1]:
-                        l+=1
-                    elif height[l+1] < height[r-1]:
-                        r-=1
-                    else:
-                        l+=1
-                else:
-                    break
-        return maxarea
-                    
+                r -= 1
+        
+        return water
