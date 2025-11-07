bb771a56b663693be8d517f00eee3b628b84fce6
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-08 01:12:16 +0300

    Time: 83 ms (39.82%) | Memory: 12.7 MB (37.74%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,17 +1,26 @@
-class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        def checkPalindrome(l, r):
-            while l < r:
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def is_palindrome(s):
+            l, r = 0, len(s) - 1
+
+            while l <= r:
                 if s[l] != s[r]:
                     return False
                 l += 1
                 r -= 1
+
             return True
 
         l, r = 0, len(s) - 1
-        while l < r:
+
+        while l <= r:
             if s[l] != s[r]:
-                return checkPalindrome(l + 1, r) or checkPalindrome(l, r - 1)
+                return is_palindrome(s[l: r]) or is_palindrome(s[l + 1: r + 1])
             l += 1
             r -= 1
+        
         return True
