6d3e46a5556d4353d078a5fb8e8d6a96a5530b6c
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-11 23:47:43 +0300

    Time: 481 ms (81.03%) | Memory: 12.5 MB (59.49%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,28 +1,31 @@
-class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
         nums.sort()
-        ans = []
+        res = []
         for i in range(len(nums) - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for j in range(i + 1, len(nums) - 2):
-                if nums[j] == nums[j - 1] and j - 1 != i:
+                if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
+                t = target - (nums[i] + nums[j])
                 l, r = j + 1, len(nums) - 1
-                res_track = target - nums[i] - nums[j]
+
                 while l < r:
-                    res_hold = nums[l] + nums[r]
-                    if res_hold < res_track:
+                    if nums[l] + nums[r] == t:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
                         l += 1
-                    elif res_hold > res_track:
                         r -= 1
-                    else:
-                        ans.append([nums[i], nums[j], nums[l], nums[r]])
-                        l += 1
-                        while l < r and nums[l] == nums[l - 1]:
+                        while l < r and l > j + 1 and nums[l] == nums[l - 1]:
                             l += 1
-        return ans
-
-
-
+                    elif nums[l] + nums[r] < t:
+                        l += 1
+                    else:
+                        r -= 1
         
+        return res
