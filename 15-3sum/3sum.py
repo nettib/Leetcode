9201df3a08674dd30a290b5bd9a26658c651b98b
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-16 02:15:00 +0300

    Time: 1031 ms (24.25%) | Memory: 18.4 MB (39.27%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,25 +1,30 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         nums.sort()
-        res = []
-        for i in range(len(nums)):
-            if i != 0 and nums[i] == nums[i - 1]:
+        ans = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             l, r = i + 1, len(nums) - 1
+
             while l < r:
-                while l < r and l - i != 1 and nums[l] == nums[l - 1]:
-                    l += 1
-                while l < r and  r != len(nums) - 1 and nums[r] == nums[r + 1]:
-                    r -= 1
-                if l == r:
-                    break
-                if nums[l] + nums[r] + nums[i] == 0:
-                    res.append([nums[i], nums[l], nums[r]])
+                if nums[i] + nums[l] + nums[r] == 0:
+                    ans.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                elif nums[l] + nums[r] + nums[i] < 0:
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                
+                elif nums[i] + nums[l] + nums[r] < 0:
                     l += 1
                 else:
                     r -= 1
-        
-        return res
+            
+        return ans
+
