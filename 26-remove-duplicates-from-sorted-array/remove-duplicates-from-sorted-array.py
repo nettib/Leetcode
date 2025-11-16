271b20437fda3cd2214225a68ef9e5d115a07a8e
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-17 00:56:56 +0300

    Time: 0 ms (100.00%) | Memory: 13.9 MB (38.52%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,8 +1,18 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         l = 1
+
         for r in range(1, len(nums)):
             if nums[r] != nums[r - 1]:
                 nums[l] = nums[r]
                 l += 1
-        return l 
+        
+        return l
+
+
+
+
