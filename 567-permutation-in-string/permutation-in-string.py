bb27fa244e8fbfd51d43f9534a3be0b3d062591c
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-21 00:31:15 +0300

    Time: 27 ms (63.41%) | Memory: 12.9 MB (11.26%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,28 +1,31 @@
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
         if len(s1) > len(s2):
             return False
-
-        s1_count = {}
-        s2_count = {}
-
-        for char in s1:
-            s1_count[char] = 1 + s1_count.get(char, 0)
+        s1Hash = {}
+        s2Hash = {}
 
         for i in range(len(s1)):
-            s2_count[s2[i]] = 1 + s2_count.get(s2[i], 0)
-        
-        if s1_count == s2_count:
-            return True
+            s1Hash[s1[i]] = s1Hash.get(s1[i], 0) + 1
 
+        for i in range(len(s1)):
+            s2Hash[s2[i]] = s2Hash.get(s2[i], 0) + 1
+        
+        if s1Hash == s2Hash:
+            return True
         
         for i in range(len(s1), len(s2)):
-            s2_count[s2[i]] = 1 + s2_count.get(s2[i], 0)
-            s2_count[s2[i - len(s1)]] -= 1
-            if s2_count[s2[i - len(s1)]] == 0:
-                del s2_count[s2[i - len(s1)]]
-
-            if s1_count == s2_count:
+            s2Hash[s2[i - len(s1)]] -= 1
+            if s2Hash[s2[i - len(s1)]] == 0:
+                del s2Hash[s2[i - len(s1)]]
+            s2Hash[s2[i]] = s2Hash.get(s2[i], 0) + 1
+            if s1Hash == s2Hash:
                 return True
         
         return False
+
