26407fb23f1f69f9dafc1ae252e468d0697ec3a5
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-21 00:53:25 +0300

    Time: 39 ms (47.11%) | Memory: 12.7 MB (82.12%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -13,18 +13,26 @@ class Solution(object):
         for i in range(len(s1)):
             s1Hash[s1[i]] = s1Hash.get(s1[i], 0) + 1
 
+        have = 0
+        need = len(s1Hash)
         for i in range(len(s1)):
             s2Hash[s2[i]] = s2Hash.get(s2[i], 0) + 1
+            if s2[i] in s1Hash and s2Hash[s2[i]] == s1Hash[s2[i]]:
+                have += 1
         
-        if s1Hash == s2Hash:
+        if have == need:
             return True
         
         for i in range(len(s1), len(s2)):
+            if s2[i - len(s1)] in s1Hash and s2Hash[s2[i - len(s1)]] == s1Hash[s2[i - len(s1)]]:
+                have -= 1
             s2Hash[s2[i - len(s1)]] -= 1
             if s2Hash[s2[i - len(s1)]] == 0:
                 del s2Hash[s2[i - len(s1)]]
             s2Hash[s2[i]] = s2Hash.get(s2[i], 0) + 1
-            if s1Hash == s2Hash:
+            if s2[i] in s1Hash and s2Hash[s2[i]] == s1Hash[s2[i]]:
+                have += 1
+            if have == need:
                 return True
         
         return False
