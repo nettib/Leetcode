529c82444a69b40a0756f4a328caf614d1ded9a8
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-20 23:59:50 +0300

    Time: 225 ms (98.66%) | Memory: 29.6 MB (12.69%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,4 +1,3 @@
-from collections import deque
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
         """
@@ -6,25 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        maxHolder = []
         q = deque([])
-        ans = []
 
-        for r in range(k):
-            while q and q[-1] < nums[r]:
-                q.pop()
-            q.append(nums[r])
-
-        ans.append(q[0])
-        l = 1
-
-        for r in range(k, len(nums)):
-            if q[0] == nums[l - 1]:
-                q.popleft()
-            while q and q[-1] < nums[r]:
-                q.pop()
-            q.append(nums[r])  
-            ans.append(q[0])
-            l += 1
+        for i in range(k):
+            while q and q[-1] < nums[i]:
+                q.pop() 
+            q.append(nums[i])
         
-        return ans
-            
+        maxHolder.append(q[0])
+
+        for i in range(k, len(nums)):
+            if nums[i - k] == q[0]:
+                q.popleft()
+            while q and q[-1] < nums[i]:
+                q.pop()
+            q.append(nums[i])
+            maxHolder.append(q[0])
+        
+        return maxHolder
