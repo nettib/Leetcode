a6dd3c64c4003750a9c757c0d419579c321421f3
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-22 01:46:46 +0300

    Time: 233 ms (18.37%) | Memory: 15.5 MB (16.34%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,16 +1,21 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        track = {}
+        longChar = -float("inf")
         l = 0
-        high = 0
+
         for r in range(len(s)):
-            count[s[r]] = count.get(s[r], 0) + 1
-            high = max(high, count[s[r]])
-            while (r - l + 1) - high > k:
-                count[s[l]] -= 1
+            track[s[r]] = track.get(s[r], 0) + 1
+
+            while (r - l) - max(track.values()) == k:
+                track[s[l]] -= 1
                 l += 1
-            res = max(res, r - l + 1)
+            
+            longChar = max(longChar, r - l + 1)
         
-        return res
+        return longChar
