1ca580befbf29349c02f438e01ed7c80b8213c85
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-11-22 01:58:27 +0300

    Time: 123 ms (77.78%) | Memory: 15.4 MB (69.11%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,13 +6,15 @@ class Solution(object):
         :rtype: int
         """
         track = {}
+        maxF = -float("inf")
         longChar = -float("inf")
         l = 0
 
         for r in range(len(s)):
             track[s[r]] = track.get(s[r], 0) + 1
+            maxF = max(maxF, track[s[r]])
 
-            while (r - l) - max(track.values()) == k:
+            while (r - l) - maxF >= k:
                 track[s[l]] -= 1
                 l += 1
             
