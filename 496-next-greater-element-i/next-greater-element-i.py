c76fb4b6e487696362d2f619d4e48f80fda778a6
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-12-16 14:33:04 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (20.67%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,18 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        idxMap = {}
+        track = { num: i for i, num in enumerate(nums1) }
+
         stack = []
+        res = [-1] * len(nums1)
 
-        for i, num in enumerate(nums1):
-            idxMap[num] = i
-
-        nums1 = [-1] * len(nums1)
-
-        for i in range(0, len(nums2)):
+        for i in range(len(nums2)):
             while stack and nums2[i] > stack[-1]:
-                nums1[idxMap[stack.pop()]] = nums2[i]
-            
-            if nums2[i] in idxMap:
+                val = stack.pop()
+                res[track[val]] = nums2[i]
+            if nums2[i] in track:
                 stack.append(nums2[i])
-
-        return nums1
+        
+        return res
