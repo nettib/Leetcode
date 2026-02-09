d3f751b717c317845fa59831364e96bccb524506
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-09 15:30:16 +0300

    Time: 0 ms (100.00%) | Memory: 13.1 MB (79.75%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,11 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        track = {}
+        nums_track = {}
 
-        for i in range(len(nums)):
-            if target - nums[i] in track:
-                return [track[target - nums[i]], i]
-            else:
-                track[nums[i]] = i
+        for i, num in enumerate(nums):
+            if target - num in nums_track:
+                return [nums_track[target - num], i]
+            nums_track[num] = i
+        
+        
         
