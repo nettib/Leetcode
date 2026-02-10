7f5844de28962ee6d26b28f3288c712f3ca11c63
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-10 14:27:47 +0300

    Time: 8 ms (81.29%) | Memory: 19.6 MB (16.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,4 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        s_hashmap = {}
-        t_hashmap = {}
-        for i in range(len(s)):
-           s_hashmap[s[i]] = 1 + s_hashmap.get(s[i], 0)
-           t_hashmap[t[i]] = 1 + t_hashmap.get(t[i], 0) 
-        if s_hashmap == t_hashmap:
-            return True
-        return False
+        return Counter(s) == Counter(t)
