e1bd4f9d788c6b2fe34f50c8915b3ea2b5a05368
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-12 15:43:21 +0300

    Time: 3 ms (33.48%) | Memory: 19.5 MB (43.31%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,7 +1,8 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if i + 1 < len(nums) and nums[i] == nums[i + 1]:
+        
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
                 nums[i] *= 2
                 nums[i + 1] = 0
         
