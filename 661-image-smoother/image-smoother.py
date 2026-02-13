45949b37f6204dc5b042dbf1b7f45f2aab585f0f
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-13 14:20:06 +0300

    Time: 271 ms (5.12%) | Memory: 20.2 MB (41.57%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,21 +1,28 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        # m * n * 9
 
-        rows, cols = len(img), len(img[0])
+        def check_row(row):
+            if row >= 0 and row < len(img):
+                return True
+            return False
 
-        out = [[0] * cols for _ in range(rows)]
+        def check_col(col):
+            if col >= 0 and col < len(img[0]):
+                return True
+            return False
+        
+        ans = [[0] * len(img[0]) for _ in range(len(img))]
 
-        for r in range(rows):
-            for c in range(cols):
-                _count = 0
-                _sum = 0
-                # process the _count and _sum
-
-                for new_row in range(max(0, r - 1), min(rows, r + 2)):
-                    for new_col in range(max(0, c - 1), min(cols, c + 2)):
-                        _count += 1
-                        _sum += img[new_row][new_col]
-
-                out[r][c] = _sum // _count
-        return out
+        for r in range(len(img)):
+            for c in range(len(img[0])):
+                total = 0
+                count = 0
+                for i in range(r - 1, r + 2):
+                    for j in range(c - 1, c + 2):
+                        if check_row(i) and check_col(j):
+                            total += img[i][j]
+                            count += 1
+                ans[r][c] = total // count
+        
+        return ans
+                
