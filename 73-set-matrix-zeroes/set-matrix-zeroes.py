eaec46a6a9377eb618550f87543979048fe26059
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-13 10:16:19 +0300

    Time: 3 ms (69.63%) | Memory: 20.2 MB (26.91%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,27 +4,19 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
 
-        # rows = [1] * len(matrix)
-        # columns = [1] * len(matrix[0])
-        left = 1
+        zero_rows = set()
+        zero_cols = set()
 
-        for row in range(len(matrix)):
-            for column in range(len(matrix[row])):
-                if matrix[row][column] == 0:
-                    matrix[row][0] = 0
-                    if column == 0:
-                        left = 0
-                    else:
-                        matrix[0][column] = 0
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 0:
+                    zero_rows.add(r)
+                    zero_cols.add(c)
         
-
-        for row in range(len(matrix) - 1, -1, -1):
-            for column in range(len(matrix[row]) - 1, -1, -1):
-                if column == 0:
-                    if matrix[row][0] == 0 or left == 0:
-                        matrix[row][column] = 0
-                else:
-                    if matrix[row][0] == 0 or matrix[0][column] == 0:
-                        matrix[row][column] = 0
-            
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if r in zero_rows or c in zero_cols:
+                    matrix[r][c] = 0
+        
+        
         
