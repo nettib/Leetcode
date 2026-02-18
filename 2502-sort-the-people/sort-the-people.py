7dc8b12de429e8f49fbcb41e811dfa27c0303d43
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-18 18:59:37 +0300

    Time: 327 ms (12.96%) | Memory: 19.5 MB (73.50%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -2,13 +2,15 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
         for i in range(len(heights)):
-            key = heights[i]
+            key1 = heights[i]
+            key2 = names[i]
             j = i - 1
 
-            while j >= 0 and heights[j] < key:
-                heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                names[j], names[j + 1] = names[j + 1], names[j]
+            while j >= 0 and heights[j] < key1:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
                 j -= 1
-            
+            heights[j + 1] = key1 
+            names[j + 1] = key2
         
         return names
