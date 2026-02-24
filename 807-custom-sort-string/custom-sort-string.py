c3b518dcfa381aaac66c7665522e65a364a3be90
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-24 19:51:03 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.58%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,15 +1,15 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        track = Counter(s)
-        order_set = set(order)
+        s_count = Counter(s)
         ans = ""
-        for char in order:
-            if char in track:
-                for _ in range(track[char]):
-                    ans += char
-        
-        for char in s:
-            if char not in order_set:
-                ans += char
 
-        return ans 
+        for char in order:
+            if char in s_count:
+                ans += (char * s_count[char])
+                s_count[char] = 0
+        
+        for char in s_count.keys():
+            if s_count[char]:
+                ans += (char * s_count[char])
+        
+        return ans
