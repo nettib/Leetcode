c18cefbd683d9cd01486ebad1335a1e1ba10e9bc
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-28 00:41:22 +0300

    Time: 507 ms (5.00%) | Memory: 19.7 MB (46.26%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,7 +7,7 @@ class Solution:
         for r in range(len(s)):
             s_count[s[r]] += 1
             
-            while (sum(s_count.values()) - max(s_count.values())) > k:
+            while (len(s[l: r + 1]) - max(s_count.values())) > k:
                 s_count[s[l]] -= 1
                 l += 1
 
