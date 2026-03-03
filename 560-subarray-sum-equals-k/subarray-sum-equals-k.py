468dc2813c128173a7212584fe8d56c652130121
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-04 00:30:33 +0300

    Time: 41 ms (26.60%) | Memory: 22 MB (55.37%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,14 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         track = {0: 1}
+
         ans = 0
-
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-
+        curr = 0
         for i in range(len(nums)):
-            if nums[i] - k in track:
-                ans += track[nums[i] - k]
-            track[nums[i]] = track.get(nums[i], 0) + 1
+            curr += nums[i]
+            if curr - k in track:
+                ans += track[curr - k]
+            
+            if curr not in track:
+                track[curr] = 0
+            track[curr] += 1
         
         return ans
