928790e3c0301def4f097cf24d2e0ef2eb2a6135
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-04 19:48:31 +0300

    Time: 93 ms (91.36%) | Memory: 33.6 MB (76.09%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,30 +1,20 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        rows = len(matrix)
-        columns = len(matrix[0])
-        for i in range(1, columns):
-            matrix[0][i] += matrix[0][i - 1]
-        for i in range(1, rows):
-            matrix[i][0] += matrix[i - 1][0]
-
-        for i in range(1, rows):
-            for j in range(1, columns):
-                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1] + matrix[i][j]
-
-        self.matrix = matrix
+        self.matrix = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                self.matrix[r + 1][c + 1] = self.matrix[r + 1][c] + self.matrix[r][c + 1] - self.matrix[r][c] + matrix[r][c]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        if row1 == 0 and col1 == 0:
-            return self.matrix[row2][col2]
-        elif row1 == 0:
-            return self.matrix[row2][col2] - self.matrix[row2][col1 - 1]
-        elif col1 == 0:
-            return self.matrix[row2][col2] - self.matrix[row1 - 1][col2]
-        else:
-            return self.matrix[row2][col2] - self.matrix[row1 - 1][col2] - self.matrix[row2][col1 - 1] + self.matrix[row1 - 1][col1 - 1]
+        top = self.matrix[row1][col2 + 1]
+        left = self.matrix[row2 + 1][col1]
+        add = self.matrix[row1][col1]
 
+        return self.matrix[row2 + 1][col2 + 1] - top - left + add
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
+
