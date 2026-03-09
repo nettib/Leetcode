f416747d4d0a416c8d5cd77a53fba82454e3db69
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-09 18:52:29 +0300

    Time: 4 ms (5.87%) | Memory: 19.4 MB (41.71%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.py
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -1,15 +1,12 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        count = 0
-        for dir in logs:
-            if dir[:len(dir)-1] == "..":
-                if count != 0:
-                    count-=1
-            elif dir[:len(dir)-1] == ".":
-                continue
-            else:
-                count+=1
-        return count
-            
+        stack = []
 
+        for log in logs:
+            if log == "../" and stack:
+                stack.pop()
+            else:
+                if log != "./" and log != "../":
+                    stack.append(log)
         
+        return len(stack)
