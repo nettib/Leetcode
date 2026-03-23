2538b4448125de033b5dc3a7dd09085a2d6ac97b
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-23 20:06:23 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (75.82%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,16 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def fastPow(x, n):
-            if n == 0:
-                return 1
-            half = fastPow(x, n // 2)  # recursive call on half
-            if n % 2 == 0:
-                return half * half
-            else:
-                return half * half * x
+        
+        def pw(x, n):
+            if n == 0: return 1
+            if n == 1: return x
 
+            res = pw(x, n // 2)
+            if n % 2 == 0:
+                return res * res
+            if n % 2 != 0:
+                return x * (res * res)
+        
         if n < 0:
-            return 1 / fastPow(x, -n)
-        else:
-            return fastPow(x, n)
-    
+            return 1 / pw(x, abs(n))
+        return pw(x, n)
+        
