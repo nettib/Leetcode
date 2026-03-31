0f3598e2c7aba17abb9b9b865427499bfe083087
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-04-01 01:57:16 +0300

    Time: 215 ms (72.57%) | Memory: 23.7 MB (96.34%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,22 +1,26 @@
-import math
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        res = float("inf")
+        def fit(capacity, weights, days):
+            _days = 1
+            _sum = 0
+
+            for weight in weights:
+                if _sum + weight > capacity:
+                    _days += 1
+                    _sum = weight
+                else:
+                    _sum += weight
+            
+            return days >= _days
+
         l, r = max(weights), sum(weights)
 
         while l <= r:
             m = l + ((r - l) // 2)
-            day = 1
-            sumWeight = 0
-            for weight in weights:
-                sumWeight += weight
-                if sumWeight > m:
-                    day += 1
-                    sumWeight = weight
-            if day <= days:
-                res = min(res, m)
-                r = m - 1
-            else:
-                l = m + 1
 
-        return res 
+            if fit(m, weights, days):
+                r = m - 1
+            else: 
+                l = m + 1
+            
+        return l
