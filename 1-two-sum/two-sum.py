8cb30ebce03537174624fe69c89739263ee809ec
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-04-05 15:38:15 +0300

    Time: 3 ms (52.95%) | Memory: 20.3 MB (66.50%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,6 +5,8 @@ class Solution:
         for i in range(len(nums)):
             if target - nums[i] in track:
                 return [track[target - nums[i]], i]
+        
             track[nums[i]] = i
-    
+
+
         
