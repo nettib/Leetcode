0eaee44461294c337c1fe782818c92ac19a98c08
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-04-08 18:31:13 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (53.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,22 +1,33 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        def get_pivot(nums):
+            l, r = 0, len(nums) - 1
 
-        while l <= r:
-            m = l + ((r - l) // 2)
-            if nums[m] == target:
-                return m
-            elif nums[l] < nums[m]:
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1
-            elif nums[l] > nums[m]:
-                if nums[m] < target < nums[l]:
+            while l < r:
+                m = l + ((r - l) // 2)
+                if nums[m] > nums[r]:
                     l = m + 1
                 else:
-                    r = m - 1
-            else:
-                l += 1
+                    r = m
+                
+            return l
         
-        return -1
+        def bs(nums, l, r, target):
+
+            while l <= r:
+                m = l + ((r - l) // 2)
+                if nums[m] == target:
+                    return m
+                elif nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return -1
+        
+        pivot = get_pivot(nums)
+        if nums[-1] < target:
+            ans = bs(nums, 0, pivot, target)
+        else:
+            ans = bs(nums, pivot, len(nums) - 1, target)
+        
+        return ans
