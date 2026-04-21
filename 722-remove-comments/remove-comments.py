198d75909a35442de409161f40dd0827f10d7c67
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-04-21 18:37:21 +0300

    Time: 3 ms (12.12%) | Memory: 19.2 MB (98.32%) - LeetSync

--- 722-remove-comments/remove-comments.py
+++ 722-remove-comments/remove-comments.py
@@ -1,39 +1,34 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:
         res = []
-        block = False
+        block = 0
         temp = ""
 
         for i in range(len(source)):
-            j = 0
             if not block:
                 temp = ""
-
+            j = 0
             while j < len(source[i]):
-                
+
                 if not block and j + 1 < len(source[i]) and source[i][j] == '/' and source[i][j + 1] == '/':
                     break
-
                 
                 if not block and j + 1 < len(source[i]) and source[i][j] == '/' and source[i][j + 1] == '*':
-                    block = True
+                    block = 1
                     j += 2
                     continue
-
-                
                 if block and j + 1 < len(source[i]) and source[i][j] == '*' and source[i][j + 1] == '/':
-                    block = False
+                    block = 0
                     j += 2
                     continue
-
                 
                 if not block:
                     temp += source[i][j]
-
                 j += 1
-
-            
+                
             if not block and temp:
                 res.append(temp)
-
+        
         return res
+
+
