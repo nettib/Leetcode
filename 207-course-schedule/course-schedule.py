5d5bca69947418592f383b1e212e40b8a986f820
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-05-07 18:20:34 +0300

    Time: 5 ms (55.52%) | Memory: 21.7 MB (6.44%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,39 +3,35 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
 
-        for courses in prerequisites:
-            c1, c2 = courses
+        for c1, c2 in prerequisites:
             graph[c2].append(c1)
         
-        colors = {}
-
-        for node in range(numCourses):
-            colors[node] = 'W'
-
+        color = [0] * numCourses
+        # 0 - white, 1 - gray, 2 - black
         visited = set()
-        cycle = False
         def dfs(node):
-            nonlocal cycle
-            visited.add(node)
-            colors[node] = 'G'
 
-            if cycle:
-                return
-
-            for neighbor in graph[node]:
-                if colors[neighbor] == 'W':
-                    dfs(neighbor)
-                elif colors[neighbor] == 'G':
-                    cycle = True
-            colors[node] = 'B'
+            for nei in graph[node]:
+                if nei in visited:
+                    if color[nei] == 1:
+                        return False
+                    continue
+                color[nei] = 1
+                visited.add(nei)
+                if not dfs(nei):
+                    return False
+                
+            color[node] = 2
+            return True
         
-        for node in range(numCourses):
-            if node not in visited:
-                dfs(node)
+        for c in range(numCourses):
+            color[c] = 1
+            visited.add(c)
+            if not dfs(c):
+                return False
         
-        return not cycle
-        
+        return True
+
+
 
         
-
-
