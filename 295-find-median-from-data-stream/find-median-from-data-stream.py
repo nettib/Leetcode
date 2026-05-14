b63ac3a246a8bea3a283a4ed0c170b20a4563249
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-05-14 19:28:29 +0300

    Time: 210 ms (32.39%) | Memory: 42.3 MB (35.18%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -7,32 +7,38 @@ class MedianFinder:
     def addNum(self, num: int) -> None:
         if len(self.large_heap) - len(self.small_heap) == 0:
             if self.small_heap:
-                _num = -heappop(self.small_heap)
+                _num = -heappop(self.small_heap) 
                 _min = min(num, _num)
                 _max = max(num, _num)
 
-                heappush(self.large_heap, _max)
-                heappush(self.small_heap, -_min)
+                heappush(self.large_heap, _max) 
+                heappush(self.small_heap, -_min) 
             else:
-                heappush(self.large_heap, num)
+                heappush(self.large_heap, num) 
         else:
-            _num = heappop(self.large_heap)
+            _num = heappop(self.large_heap) 
             _min = min(num, _num)
             _max = max(num, _num)
 
-            heappush(self.large_heap, _max)
-            heappush(self.small_heap, -_min)
-
+            heappush(self.large_heap, _max) 
+            heappush(self.small_heap, -_min) 
 
     def findMedian(self) -> float:
-        m1 = heappop(self.large_heap)
-        heappush(self.large_heap, m1)
+        m1 = self.large_heap[0]
         if len(self.large_heap) - len(self.small_heap) == 0:
-            m2 = -heappop(self.small_heap)
-            heappush(self.small_heap, -m2)
+            m2 = -self.small_heap[0]
             return (m1 + m2) / 2
         else:
             return m1
+        # m1 = heappop(self.large_heap) 
+        # heappush(self.large_heap, m1) 
+        # if len(self.large_heap) - len(self.small_heap) == 0:
+        #     m2 = -heappop(self.small_heap) 
+        #     heappush(self.small_heap, -m2) 
+        #     return (m1 + m2) / 2
+        # else:
+        #     return m1
+    
 
         
 
