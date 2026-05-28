3f50c5fce7ec1a0af6dfd3fc6d47b8cdde9dd681
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-05-28 23:17:36 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.02%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        nums[i+1:] = reversed(nums[i+1:])
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
