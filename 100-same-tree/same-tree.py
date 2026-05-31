a9ead0865679cba765b4005f5f4b9ed4bc8349a6
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-05-31 23:32:57 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (94.39%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -20,3 +20,8 @@ class Solution:
                 
         return isSameTree(p, q)
 
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
