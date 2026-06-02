6355bb5ce7ebda7250e306052d8bd6f5ed2b71fe
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-02 23:37:03 +0300

    Time: 0 ms (100.00%) | Memory: 20.8 MB (94.98%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -18,3 +18,8 @@ class Solution:
                     validateBST(node.right, max(lb, node.val), rb))
                 
         return validateBST(root, -float("inf"), float("inf"))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
