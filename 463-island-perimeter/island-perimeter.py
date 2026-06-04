d5f62a89fd133faa854fd873a290417b47b54018
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-05 00:28:15 +0300

    Time: 143 ms (14.58%) | Memory: 33.6 MB (15.78%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -2,38 +2,33 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
-        def inbound(r, c):
+        def inbound(r: int, c: int) -> bool:
             return 0 <= r < len(grid) and 0 <= c < len(grid[0])
 
-        # Get starting point or land
-        start = None
+        visited = set()
+        perimeter = 0
+
+        def dfs(r, c):
+            nonlocal perimeter
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if (nr, nc) in visited:
+                    continue
+                if not inbound(nr, nc) or grid[nr][nc] == 0:
+                    perimeter += 1
+                else:
+                    visited.add((nr, nc))
+                    dfs(nr, nc)
+
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    start = [r, c]
-                    break
-            if start:
-                break
+                    visited.add((r, c))
+                    dfs(r, c)
+                    return perimeter
 
-          
-        # dfs
-        perimeter = 0
-        visited = set()
 
-        visited.add(tuple(start))
-        
-        def dfs(r, c):
-            nonlocal perimeter
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if not inbound(nr, nc) or grid[nr][nc] == 0:
-                    perimeter += 1
-                    continue
-                if (nr, nc) in visited:
-                    continue
-                visited.add((nr, nc))
-                dfs(nr, nc)
-
-        dfs(*start)
-        return perimeter
-                
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
