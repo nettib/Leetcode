ef0a0036f76552647a97778f573c9c9f94b2cb81
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-05 00:49:58 +0300

    Time: 253 ms (39.33%) | Memory: 31.3 MB (5.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,25 +2,31 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
-        def inbound(r, c):
+        def inbound(r: int, c: int) -> bool:
             return 0 <= r < len(grid) and 0 <= c < len(grid[0])
-        
-        visited = set()
+
         def dfs(r, c):
 
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if not inbound(nr, nc) or grid[nr][nc] == "0": continue
-                if (nr, nc) in visited: continue
-                visited.add((nr, nc))
-                dfs(nr, nc)
-        
+                if (nr, nc) in visited or not inbound(nr, nc):
+                    continue
+
+                if grid[nr][nc] == "1":
+                    visited.add((nr, nc))
+                    dfs(nr, nc)
+
+        visited = set()
         islands = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if (r, c) in visited or grid[r][c] == "0": continue
-                islands += 1
-                visited.add((r, c))
-                dfs(r, c)
-        
-        return islands 
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    visited.add((r, c))
+                    islands += 1
+                    dfs(r, c)
+
+        return islands
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
