4b3246ae650c95233cb43e78262d25a9234bc386
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-08 11:03:10 +0300

    Time: 23 ms (84.61%) | Memory: 33.6 MB (77.63%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,36 +1,24 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        res = [0] * len(nums)
-        i1 = 0
-        j1 = len(nums) - 1
-        for i in range(len(nums)):
-            if nums[i] < pivot:
-                res[i1] = nums[i]
-                i1 += 1
-        for j in range(len(nums) - 1, -1, -1):
-            if nums[j] > pivot:
-                res[j1] = nums[j]
-                j1 -= 1
-        for k in range(i1, j1 + 1):
-            res[k] = pivot
-        
-        return res
+        l = []
+        e = []
+        g = []
 
+        for num in nums:
+            if num < pivot:
+                l.append(num)
+            elif num > pivot:
+                g.append(num)
+            else:
+                e.append(num)
 
+        ans = []
+        ans.extend(l)
+        ans.extend(e)
+        ans.extend(g)
 
+        return ans
 
-
-
-
-        # small = [] 
-        # large = []
-        # equal = []
-        # for i in range(len(nums)):
-        #     if nums[i] < pivot:
-        #         small.append(nums[i])
-        #     elif nums[i] > pivot:
-        #         large.append(nums[i])
-        #     else:
-        #         equal.append(nums[i])
-        # nums = small + equal + large
-        # return nums
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
