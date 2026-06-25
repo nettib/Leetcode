6b782b0fe1299c1548430cac5ba76fa5f6cbaf5d
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-25 23:33:41 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (86.41%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
