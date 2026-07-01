77eeb36a19bba3c80de131f3d4ed7774d396fffc
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-07-02 00:35:41 +0300

    Time: 59 ms (33.22%) | Memory: 26.1 MB (65.93%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -2,39 +2,32 @@ class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         _graph = defaultdict(list)
         degree = [0] * len(graph)
-        safe = [0] * len(graph)
 
-        for i in range(len(graph)):
-            for node in graph[i]:
-                _graph[node].append(i)
-            degree[i] += len(graph[i])
+        for n1 in range(len(graph)):
+            for n2 in graph[n1]:
+                _graph[n2].append(n1)
+                degree[n1] += 1
 
-        
-        q = deque()
+        q = deque([])
 
         for node in range(len(degree)):
-            if degree[node] == 0: 
+            if degree[node] == 0:
                 q.append(node)
-        
-        while q:
-            node = q.popleft()
-            safe[node] = 1
-
-            for nei in _graph[node]:
-                degree[nei] -= 1
-                if degree[nei] == 0:
-                    q.append(nei)
         
         ans = []
 
-        for node in range(len(safe)):
-            if safe[node]:
-                ans.append(node)
-        
-        return ans
-                
+        while q:
+            node = q.popleft()
+            ans.append(node)
 
+            for nei in _graph[node]:
+                degree[nei] -= 1
+
+                if degree[nei] == 0:
+                    q.append(nei)
+            
+        return sorted(ans)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
