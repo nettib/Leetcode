bf345b7a2358c54501cca75fa6ffbfbe7f9a8679
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-07-11 07:18:56 +0300

    Time: 0 ms (100.00%) | Memory: 20.7 MB (10.36%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,35 +1,33 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        colors = [-1] * len(graph)
-
-        visited = set()
+        color = [-1] * len(graph)
 
         def dfs(node):
 
             for nei in graph[node]:
                 if nei in visited:
-                    if colors[node] == colors[nei]:
+                    if color[nei] != -1 and color[node] == color[nei]:
                         return False
-                else:
-                    colors[nei] = 1 - colors[node]
-                    visited.add(nei)
-                    if not dfs(nei):
-                        return False
-
+                    continue
+                
+                visited.add(nei)
+                color[nei] = 1 - color[node]
+                if not dfs(nei):
+                    return False
+            
             return True
-
+    
+        visited = set()
         for node in range(len(graph)):
             if node not in visited:
-                colors[node] = 0
                 visited.add(node)
+                color[node] = 0
                 if not dfs(node):
                     return False
-
+        
         return True
-
-
 
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
