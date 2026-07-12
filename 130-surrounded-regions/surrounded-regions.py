a31fb03b7301b21353c13a1d6fb3692ee922a7bd
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-07-12 05:14:53 +0300

    Time: 59 ms (5.47%) | Memory: 40.1 MB (5.01%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,40 +1,42 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
         def inbound(r, c):
             return 0 <= r < len(board) and 0 <= c < len(board[0])
-
-        def board_edge(r, c, board):
-            return (
-                (0 == r)
-                or (r == (len(board) - 1))
-                or (0 == c)
-                or (c == (len(board[0]) - 1))
-            )
+        
+        def at_the_edge(r, c):
+            return 0 == r or 0 == c or len(board) - 1 == r or len(board[0]) - 1 == c
 
         def dfs(r, c):
+            nonlocal border
 
-            board[r][c] = "U"
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-
-                if not inbound(nr, nc) or board[nr][nc] != "O":
+                if not inbound(nr, nc) or (nr, nc) in visited or board[nr][nc] == 'X':
                     continue
-
+                if at_the_edge(nr, nc):
+                    border = 1
+                
+                visited.add((nr, nc))
+                temp.append((nr, nc))
                 dfs(nr, nc)
-
+                    
+        visited = set()
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if board_edge(r, c, board) and board[r][c] == "O":
+                if board[r][c] == 'O' and (r, c) not in visited and not at_the_edge(r, c):
+                    visited.add((r, c))
+                    temp = [(r, c)]
+                    border = 0
                     dfs(r, c)
-
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "U":
-                    board[r][c] = "O"
+                    if not border:
+                        for rr, cc in temp:
+                            board[rr][cc] = 'X'
+                        
 
 
 # Synced seamlessly with LeetHub Pro
