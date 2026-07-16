66b00b93f16d8001f76d43fb00d517fccbd629c1
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-07-16 05:45:56 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (17.65%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,10 @@ class Solution:
         for i in range(len(nums)):
             if target - nums[i] in track:
                 return [track[target - nums[i]], i]
-        
             track[nums[i]] = i
-
-
         
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
