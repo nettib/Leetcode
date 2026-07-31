581fc49848396acee86c956b51a5e17fc58524f5
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-07-31 06:15:18 +0300

    Time: 15 ms (11.20%) | Memory: 20.3 MB (69.82%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,29 +1,44 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = {i: i for i in range(size)}
+        self.rank = [0] * size
+    
+    def find(self, x):
+        if x == self.parent[x]:
+            return x
+
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+
+        if px != py:
+            if self.rank[px] == self.rank[py]:
+                self.parent[py] = px
+                self.rank[px] += 1
+            elif self.rank[px] > self.rank[py]:
+                self.parent[py] = px
+            else:
+                self.parent[px] = py
+
+
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        graph = UnionFind(len(grid))
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == 1:
-                    graph[r].append(c)
-                
-
-        def dfs(node):
-            visited.add(node)
-
-            for nei in graph[node]:
-                if nei in visited:
-                    continue
-                dfs(nei)
+                if r != c and grid[r][c] == 1:
+                    graph.union(r, c)
         
-        visited = set()
-        provinces = 0
+        track = set()
         for node in range(len(grid)):
-            if node not in visited:
-                dfs(node)
-                provinces += 1
+            track.add(graph.find(node))
         
-        return provinces
+        return len(track)
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
