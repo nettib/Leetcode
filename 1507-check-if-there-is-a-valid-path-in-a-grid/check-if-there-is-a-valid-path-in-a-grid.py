a528f0f5c08c51b97d5e1c074aa05127fd79619a
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-08-02 14:35:48 +0300

    Time: 1380 ms (5.16%) | Memory: 48.3 MB (30.47%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,5 +1,42 @@
+class DSU:
+    def __init__(self, size):
+        self.parent = {i: i for i in range(size)}
+        self.size = [1] * size
+
+    def find(self, node):
+        if node == self.parent[node]:
+            return node
+        
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, n1, n2):
+        pn1 = self.find(n1)
+        pn2 = self.find(n2)
+
+        if pn1 != pn2:
+            if self.size[pn1] > self.size[pn2]:
+                self.parent[pn2] = pn1
+                self.size[pn1] += self.size[pn2]
+            else:
+                self.parent[pn1] = pn2
+                self.size[pn2] += self.size[pn1]
+    
+    def is_connected(self, n1, n2):
+        return self.find(n1) == self.find(n2)
+
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
+        _id = {}
+
+        i = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                _id[(r, c)] = i
+                i += 1
+
+        dsu = DSU(len(grid) * len(grid[0]))
+
         UP = [-1, 0]
         DOWN = [1, 0]
         LEFT = [0, -1]
@@ -19,37 +56,30 @@ class Solution:
         def inbound(r, c):
             return 0 <= r < len(grid) and 0 <= c < len(grid[0])
 
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                _dir = directions[grid[r][c]]
 
-        def dfs(r, c, d):
-            _dir = directions[grid[r][c]]
+                for dr, dc in _dir:
+                    nr, nc = dr + r, dc + c
 
-            if d and complement[d] not in _dir:
-                return False
+                    if not inbound(nr, nc):
+                        continue
+                    
+                    _dir2 = directions[grid[nr][nc]]
 
-            visited.add((r, c))
-            if r == len(grid) - 1 and c == len(grid[0]) - 1:
-                return True
+                    if complement[(dr, dc)] in _dir2:
+                        n1 = _id[(r, c)]
+                        n2 = _id[(nr, nc)]
+                        dsu.union(n1, n2)
+                    
+        return dsu.is_connected(_id[(0, 0)], _id[(len(grid) - 1, len(grid[0]) - 1)])
 
-            for dr, dc in _dir:
-                nr, nc = dr + r, dc + c
 
-                if (nr, nc) in visited or not inbound(nr, nc):
-                    continue
-                if dfs(nr, nc, (dr, dc)):
-                    return True
-            
-            return False
+
+
         
-        visited = set()
-        return dfs(0, 0, None)
-                
-
-
-            
-
-
-
-
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
