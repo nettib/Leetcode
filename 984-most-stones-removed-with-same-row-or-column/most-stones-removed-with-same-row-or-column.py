583be039ff6023c64179d62218dcdff58733f180
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-08-04 00:33:05 +0300

    Time: 19 ms (73.21%) | Memory: 19.8 MB (75.40%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -2,6 +2,7 @@ class DSU:
     def __init__(self, size):
         self.parent = {i : i for i in range(size)}
         self.size = [1] * size
+        self.components = size
 
     def find(self, stone):
         if stone == self.parent[stone]:
@@ -15,34 +16,22 @@ class DSU:
         ps2 = self.find(s2)
 
         if ps1 != ps2:
+            self.components -= 1
             if self.size[ps1] > self.size[ps2]:
                 self.parent[ps2] = ps1
                 self.size[ps1] += self.size[ps2]
             else:
                 self.parent[ps1] = ps2
                 self.size[ps2] += self.size[ps1]
-        
-    def get_groups(self):
-        ans = 0
-        for stone in self.parent:
-            if stone == self.parent[stone]:
-                ans += 1
-            
-        return ans
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         row = defaultdict(list)
-
-        for stone, position in enumerate(stones):
-            x, y = position
-
-            row[x].append(stone)
-        
         col = defaultdict(list)
 
         for stone, position in enumerate(stones):
             x, y = position
 
+            row[x].append(stone)
             col[y].append(stone)
         
         dsu = DSU(len(stones))
@@ -56,7 +45,7 @@ class Solution:
             for i in range(1, len(col[c])):
                 dsu.union(col[c][i], col[c][i - 1])
 
-        return len(stones) - dsu.get_groups()
+        return len(stones) - dsu.components
 
         
 
