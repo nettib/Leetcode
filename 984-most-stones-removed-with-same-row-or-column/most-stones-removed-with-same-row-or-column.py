536e246cbaab551853e6dcaffc73987835fd5ef5
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-08-04 00:26:34 +0300

    Time: 13 ms (89.34%) | Memory: 19.8 MB (68.18%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,69 @@
+class DSU:
+    def __init__(self, size):
+        self.parent = {i : i for i in range(size)}
+        self.size = [1] * size
+
+    def find(self, stone):
+        if stone == self.parent[stone]:
+            return stone
+        
+        self.parent[stone] = self.find(self.parent[stone])
+        return self.parent[stone]
+    
+    def union(self, s1, s2):
+        ps1 = self.find(s1)
+        ps2 = self.find(s2)
+
+        if ps1 != ps2:
+            if self.size[ps1] > self.size[ps2]:
+                self.parent[ps2] = ps1
+                self.size[ps1] += self.size[ps2]
+            else:
+                self.parent[ps1] = ps2
+                self.size[ps2] += self.size[ps1]
+        
+    def get_groups(self):
+        ans = 0
+        for stone in self.parent:
+            if stone == self.parent[stone]:
+                ans += 1
+            
+        return ans
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        row = defaultdict(list)
+
+        for stone, position in enumerate(stones):
+            x, y = position
+
+            row[x].append(stone)
+        
+        col = defaultdict(list)
+
+        for stone, position in enumerate(stones):
+            x, y = position
+
+            col[y].append(stone)
+        
+        dsu = DSU(len(stones))
+
+
+        for r in row:
+            for i in range(1, len(row[r])):
+                dsu.union(row[r][i], row[r][i - 1])
+        
+        for c in col:
+            for i in range(1, len(col[c])):
+                dsu.union(col[c][i], col[c][i - 1])
+
+        return len(stones) - dsu.get_groups()
+
+        
+
+
+            
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
