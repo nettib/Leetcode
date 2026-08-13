148916cda142dcf02a30e5f8db17db1e1f3bebf8
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-08-13 03:04:36 +0300

    Time: 27 ms (75.56%) | Memory: 24.6 MB (16.85%) - LeetSync

---
+++ 922-possible-bipartition/possible-bipartition.py
@@ -0,0 +1,41 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        groups = [-1] * (n + 1)
+        graph = defaultdict(list)
+
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def dfs(node):
+            visited.add(node)
+
+            for nei in graph[node]:
+                if nei in visited:
+                    if groups[nei] == groups[node]:
+                        return False
+                    continue
+
+                if groups[node]:
+                    groups[nei] = 0
+                else:
+                    groups[nei] = 1
+
+                if not dfs(nei):
+                    return False
+
+            return True
+
+        visited = set()
+
+        for node in range(1, n + 1):
+            if node not in visited:
+                groups[node] = 1
+                if not dfs(node):
+                    return False
+        
+        return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
