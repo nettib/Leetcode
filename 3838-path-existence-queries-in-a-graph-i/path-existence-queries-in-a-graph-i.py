cb8415ec3b6d149760a618750efd3f86961166dc
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-08-31 11:04:33 +0300

    Time: 79 ms (61.37%) | Memory: 50.4 MB (28.98%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -1,50 +1,21 @@
-class DSU:
-    def __init__(self, size):
-        self.parent = {node: node for node in range(size)}
-        self.size = [1] * size
-    
-    def get_parent(self, node):
-        if self.parent[node] == node:
-            return node
-        
-        self.parent[node] = self.get_parent(self.parent[node])
-        return self.parent[node]
-
-    def union(self, n1, n2):
-        pn1 = self.get_parent(n1)
-        pn2 = self.get_parent(n2)
-    
-        if pn1 != pn2:
-            if self.size[pn1] > self.size[pn2]:
-                self.parent[pn2] = pn1
-                self.size[pn1] += self.size[pn2]
-            else:
-                self.parent[pn1] = pn2
-                self.size[pn2] += self.size[pn1]
-    
-    def is_connected(self, n1, n2):
-        pn1 = self.get_parent(n1)
-        pn2 = self.get_parent(n2)
-    
-        return pn1 == pn2
-
-
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        dsu = DSU(n)
-        for i in range(1, n):
-            if abs(nums[i] - nums[i - 1]) <= maxDiff:
-                dsu.union(i, i - 1)
+        component = [0] * n
+
+        comp = 0
+
+        for i in range(n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                comp += 1
+            
+            component[i] = comp
         
         ans = []
         for n1, n2 in queries:
-            ans.append(dsu.is_connected(n1, n2))
+            ans.append(component[n1] == component[n2])
         
-
         return ans
 
-
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
