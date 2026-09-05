bb6c2488ad85c50014f858f134208be1d27c9a56
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-09-05 23:15:17 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.96%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -13,3 +13,8 @@ class Solution:
                     stack.pop()
 
         return len(stack) == 0
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
