93974665ac5c2a71aeeab2951f70d715181a1f6b
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-09-16 11:05:20 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (60.54%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,22 +1,27 @@
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subsets = []
+    def subsetsWithDup(self, nums: list[int]) -> list[list[int]]:
+        ans = []
 
         nums.sort()
 
-        def backtrack(i):
-            if i >= len(nums):
-                res.append(subsets[:])
+        def backtrack(i, path):
+            if i == len(nums):
+                ans.append(path[:])
                 return
             
-            subsets.append(nums[i])
-            backtrack(i + 1)
-            subsets.pop()
+
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
 
             while i < len(nums) - 1 and nums[i] == nums[i + 1]:
                 i += 1
-            backtrack(i + 1)
-        
-        backtrack(0)
-        return res
+            backtrack(i + 1, path)
+
+        backtrack(0, [])
+
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
